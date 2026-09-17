7ef2dccd87db00ffab5813578d039da34d8e7df1
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-09-18 00:10:21 +0530

    Modifing Project Images and Leetcode Link

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@
     if (typeof Lenis === 'undefined' || prefersReducedMotion) return;
 
     lenis = new Lenis({
-      duration: 3.5,
+      duration: 2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: 'vertical',
       gestureOrientation: 'vertical',
